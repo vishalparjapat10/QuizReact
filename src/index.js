c3e437bdd02d224a47bc645c13df228b04e96ad2
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-05-14 15:59:20 +0530

    questions fetched & shown on UI

--- src/index.js
+++ src/index.js
@@ -15,10 +15,16 @@ class QuizBee extends Component{
             })
         })
     }
+
+    componentDidMount(){
+        // this function will be called when this component will be load for the first time
+        this.getQuestions();
+    }
     render(){
         return(
             <div className="container">
                 <div className="title">QuizBee</div>
+                {this.state.questionBank.length > 0 && this.state.questionBank.map(({question,answers,correct,questionId}) => (<h4>{question}</h4>))}
             </div>
         );
     }
